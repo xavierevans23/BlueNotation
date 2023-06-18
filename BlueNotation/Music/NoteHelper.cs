3768e9fbb8b0413e1c9d7983a13b4e3fed52f8e9
Xavier Evans <xavierevans341@gmail.com>
2023-06-18 21:24:46 +0100

    Added method to get appriopate name for note in a given scale.

--- BlueNotation/Music/NoteHelper.cs
+++ BlueNotation/Music/NoteHelper.cs
@@ -18,12 +18,35 @@ public static class NoteHelper
 
     public static Note GetNote(int midi, Key key)
     {
-        throw new NotImplementedException();
+        var offset = midi % 12;
+
+        if (offset < 0)
+        {
+            offset += 12;
+        }
+
+        if (!_scaleNotes.ContainsKey(key))
+        {
+            throw new ArgumentException($"There's no major key \"{key}\".");
+        }
+
+        if (!_scaleNotes[key].ContainsKey(offset))
+        {
+            return GetNote(midi);
+        }
+        
+        var octave = ((midi - offset) / 12) - 1;
+
+        var note = _scaleNotes[key][offset];
+
+        note = note with { Octave = octave + note.Octave };
+
+        return note;
     }
 
     public static int GetMidi(Note note)
     {
-        throw new NotImplementedException();
+        return _letterOffsets[note.Letter] + ((note.Octave + 1) * 12) + _accidentalOffsets[note.Accidental];
     }
 
     public static IEnumerable<int> GetScale(Letter startLetter)
@@ -58,6 +81,108 @@ public static class NoteHelper
         Accidental.Flat,
         Accidental.Natural,
         Accidental.Flat,
-        Accidental.Natural,
+        Accidental.Natural
+    };
+
+    private static readonly Dictionary<Letter, int> _letterOffsets = new() {
+        { Letter.C, 0},
+        { Letter.D, 2},
+        { Letter.E, 4},
+        { Letter.F, 5},
+        { Letter.G, 7},
+        { Letter.A, 9},
+        { Letter.B, 11}
+    };
+
+    private static readonly Dictionary<Accidental, int> _accidentalOffsets = new() {
+        { Accidental.Natural, 0},
+        { Accidental.Sharp, 1},
+        { Accidental.Flat, -1}
+    };
+
+    static NoteHelper()
+    {
+        foreach (var scale in _majorScaleData)
+        {
+            var keyStrings = scale.Split(' ');
+            var keys = new List<Key>();
+
+            foreach (var key in keyStrings)
+            {
+                keys.Add(new(_letterChars[key[0]], _accidentalChars[key[1]]));
+            }
+
+            var first = keys.First();
+            var midiStart = GetMidi(new(first.Letter, 0, first.Accidental));
+
+            _scaleNotes.Add(first, new());
+
+            for (int i = 0; i < 7; i++)
+            {
+                var midi = midiStart + _majorScaleSteps[i];
+
+                var offset = midi % 12;
+
+                if (offset < 0)
+                {
+                    offset += 12;
+                }
+
+                var key = keys[i];
+                var octave = 0;
+
+                if (key.Letter == Letter.C && key.Accidental == Accidental.Flat)
+                {
+                    octave++;
+                }
+                if (key.Letter == Letter.B && key.Accidental == Accidental.Sharp)
+                {
+                    octave--;
+                }
+
+                var note = new Note(key.Letter, octave, key.Accidental);
+
+                _scaleNotes[first][offset] = note;
+            }
+        }
+    }
+
+    private static readonly Dictionary<Key, Dictionary<int, Note>> _scaleNotes = new();
+
+    private static readonly int[] _majorScaleSteps = new[] { 0, 2, 4, 5, 7, 9, 11 };
+
+    private static readonly string[] _majorScaleData = new[]
+    {
+        "Cn Dn En Fn Gn An Bn",
+        "Gn An Bn Cn Dn En F#",
+        "Dn En F# Gn An Bn C#",
+        "An Bn C# Dn En F# G#",
+        "En F# G# An Bn C# D#",
+        "Fn Gn An Bb Cn Dn En",
+        "Bb Cn Dn Eb Fn Gn An",
+        "Eb Fn Gn Ab Bb Cn Dn",
+        "Ab Bb Cn Db Eb Fn Gn",
+        "Bn C# D# En F# G# A#",
+        "Cb Db Eb Fb Gb Ab Bb",
+        "F# G# A# Bn C# D# E#",
+        "Gb Ab Bb Cb Db Eb Fn",
+        "C# D# E# F# G# A# B#",
+        "Db Eb Fn Gb Ab Bb Cn",
+    };
+
+    private static readonly Dictionary<char, Letter> _letterChars = new() {
+        { 'C', Letter.C},
+        { 'D', Letter.D},
+        { 'E', Letter.E},
+        { 'F', Letter.F},
+        { 'G', Letter.G},
+        { 'A', Letter.A},
+        { 'B', Letter.B}
+    };
+
+    private static readonly Dictionary<char, Accidental> _accidentalChars = new() {
+        { 'n', Accidental.Natural},
+        { '#', Accidental.Sharp},
+        { 'b', Accidental.Flat}
     };
 }
--- BlueNotationTests/NoteHelperTest.cs
+++ BlueNotationTests/NoteHelperTest.cs
@@ -30,13 +30,46 @@ public class NoteHelperTest
     [Fact]
     public void TestGetNoteWithKey()
     {
+        var cMajor = new Key(Letter.C, Accidental.Natural);
 
+        Assert.Equal(new Note(Letter.C, 4, Accidental.Natural), NoteHelper.GetNote(60, cMajor));
+        Assert.Equal(new Note(Letter.C, 4, Accidental.Sharp), NoteHelper.GetNote(61, cMajor));
+        Assert.Equal(new Note(Letter.B, 4, Accidental.Natural), NoteHelper.GetNote(71, cMajor));
+        Assert.Equal(new Note(Letter.E, 4, Accidental.Flat), NoteHelper.GetNote(63, cMajor));        
+
+        var cFlatMajor = new Key(Letter.C, Accidental.Flat);
+
+        Assert.Equal(new Note(Letter.C, 5, Accidental.Flat), NoteHelper.GetNote(71, cFlatMajor));
+        Assert.Equal(new Note(Letter.C, 5, Accidental.Natural), NoteHelper.GetNote(72, cFlatMajor));
+        
+        var cSharpMajor = new Key(Letter.C, Accidental.Sharp);
+
+        Assert.Equal(new Note(Letter.C, 4, Accidental.Sharp), NoteHelper.GetNote(61, cSharpMajor));
+        Assert.Equal(new Note(Letter.B, 3, Accidental.Sharp), NoteHelper.GetNote(60, cSharpMajor));
+        Assert.Equal(new Note(Letter.E, 4, Accidental.Sharp), NoteHelper.GetNote(65, cSharpMajor));
+        Assert.Equal(new Note(Letter.D, 4, Accidental.Natural), NoteHelper.GetNote(62, cSharpMajor));
+
+        var dMajor = new Key(Letter.D, Accidental.Natural);
+
+        Assert.Equal(new Note(Letter.C, 4, Accidental.Sharp), NoteHelper.GetNote(61, dMajor));
+        Assert.Equal(new Note(Letter.C, 4, Accidental.Natural), NoteHelper.GetNote(60, dMajor));
+        Assert.Equal(new Note(Letter.F, 4, Accidental.Natural), NoteHelper.GetNote(65, dMajor));
+        Assert.Equal(new Note(Letter.E, 4, Accidental.Flat), NoteHelper.GetNote(63, dMajor));
+
+        Assert.Throws<ArgumentException>(() => NoteHelper.GetNote(60, new Key(Letter.D, Accidental.Sharp)));
+        Assert.Throws<ArgumentException>(() => NoteHelper.GetNote(60, new Key(Letter.A, Accidental.Sharp)));
+        Assert.Throws<ArgumentException>(() => NoteHelper.GetNote(60, new Key(Letter.F, Accidental.Flat)));
     }
 
     [Fact]
     public void TestGetMidi()
     {
-
+        Assert.Equal(60, NoteHelper.GetMidi(new(Letter.C, 4, Accidental.Natural)));
+        Assert.Equal(0, NoteHelper.GetMidi(new(Letter.C, -1, Accidental.Natural)));
+        Assert.Equal(102, NoteHelper.GetMidi(new(Letter.F, 7, Accidental.Sharp)));
+        Assert.Equal(40, NoteHelper.GetMidi(new(Letter.E, 2, Accidental.Natural)));
+        Assert.Equal(60, NoteHelper.GetMidi(new(Letter.B, 3, Accidental.Sharp)));
+        Assert.Equal(59, NoteHelper.GetMidi(new(Letter.C, 4, Accidental.Flat)));
     }
 
     [Fact]
