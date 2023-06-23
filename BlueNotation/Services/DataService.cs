1f3996a649b34861f4015ac88ee6a91d48d41d89
Xavier Evans <xavierevans341@gmail.com>
2023-06-23 22:48:17 +0100

    Added data service.

---
+++ BlueNotation/Services/DataService.cs
@@ -0,0 +1,38 @@
+﻿using BlueNotation.Data;
+using System;
+using System.Xml.Serialization;
+
+namespace BlueNotation.Services;
+
+public class DataService
+{
+    public Statistics Statistics { get; private set; } = new();
+
+    public string SerializeData() 
+    {
+        Statistics.LoadData();
+
+        var serializer = new XmlSerializer(Statistics.GetType());
+
+        using StringWriter textWriter = new();        
+        serializer.Serialize(textWriter, Statistics);
+        
+        return textWriter.ToString();
+    }
+
+    public void DeserializeData(string xml) 
+    {
+        using var reader = new StringReader(xml);
+        var deserializer = new XmlSerializer(Statistics.GetType());
+
+        if (deserializer.Deserialize(reader) is Statistics stats) 
+        {
+            Statistics = stats;
+            
+            Statistics.UnloadData();
+            return;
+        }
+
+        throw new ArgumentException("Could not be deserialized.", nameof(xml));
+    }
+}
---
+++ BlueNotationTests/Services/DataServiceTest.cs
@@ -0,0 +1,60 @@
+﻿using BlueNotation.Data;
+using BlueNotation.Game;
+using BlueNotation.Music;
+using BlueNotation.Services;
+
+namespace BlueNotationTests.Services;
+
+public class DataServiceTest
+{
+    [Fact]
+    public void SerializationTest()
+    {
+        var preset = new NotesSessionPreset
+        {
+            TrebleNoteRange = new() { 60, 62 },
+            ClefMode = ClefMode.Treble,
+            AllowRepeats = false,
+            MaxNotes = 1,
+            MinNotes = 1
+        };
+
+        var session = new NotesSession(preset);
+        var note = session.GetNotes().First();
+
+        session.NotePlayed(NoteHelper.GetMidi(note), 100);
+        session.NotePlayed(NoteHelper.GetMidi(note), 100);
+
+        note = session.GetNotes().First();
+        session.NotePlayed(NoteHelper.GetMidi(note), 200);
+
+        note = session.GetNotes().First();
+        session.NotePlayed(NoteHelper.GetMidi(note), 50);
+
+        var dataService = new DataService();
+
+        var stats = dataService.Statistics;
+        stats.TotalNotesAttempted = 2;
+        stats.TotalNotesPlayed = 1;
+
+        session.ApplyStatistics(stats);
+
+        var xml = dataService.SerializeData();
+        dataService.DeserializeData(xml);
+        stats = dataService.Statistics;
+
+        var firstMidi = NoteHelper.GetMidi(note);
+        Assert.Equal(6, stats.TotalNotesAttempted);
+        Assert.Equal(4, stats.TotalNotesPlayed);
+
+        Assert.Equal(2, stats.GetNote(firstMidi).TotalAttempts);
+        Assert.Equal(2, stats.GetNote(firstMidi).TotalTimesPlayed);
+        Assert.Equal(150, stats.GetNote(firstMidi).TotalLatency);
+
+        firstMidi = firstMidi == 60 ? 62 : 60;
+
+        Assert.Equal(2, stats.GetNote(firstMidi).TotalAttempts);
+        Assert.Equal(1, stats.GetNote(firstMidi).TotalTimesPlayed);
+        Assert.Equal(200, stats.GetNote(firstMidi).TotalLatency);
+    }
+}
