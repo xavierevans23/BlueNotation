f324c494e1ed57297615f41546f881bd13cd06bf
Xavier Evans <xavierevans341@gmail.com>
2023-06-24 01:21:52 +0100

    Added midi service.

---
+++ BlueNotation/Services/MidiService.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.JSInterop;
+
+namespace BlueNotation.Services;
+
+public class MidiService
+{
+    private readonly IJSRuntime _jsRuntime;    
+    private bool _started = false;
+    
+    private static MidiService? _instance;
+
+    public MidiStatus MidiStatus { get; private set; } = MidiStatus.Ready;
+    public string? DeviceName { get; private set; }
+    public string? MidiError { get; private set; }
+
+    public event EventHandler<int>? NoteDown;
+    public event EventHandler? StatusChanged;    
+
+    public MidiService(IJSRuntime jsRuntime) 
+    {
+        _jsRuntime = jsRuntime;
+        _instance = this;
+    }
+
+    public async Task StartConnection()
+    {
+        if (_started) return;
+
+        _started = true;
+        await _jsRuntime.InvokeVoidAsync("midiStart");
+    }
+
+    [JSInvokable]
+    public static void MidiEvent(string midiStatus, string deviceName, string midiError)
+    {
+        if (_instance is null) return;
+
+        _instance.DeviceName = null;
+        _instance.MidiError = null;
+
+        switch (midiStatus)
+        {
+            case "ready":
+                _instance.MidiStatus = MidiStatus.Ready;                
+                break;
+            case "unsupported":
+                    _instance.MidiStatus = MidiStatus.NotSupported;
+                break;
+            case "error":
+                _instance.MidiStatus = MidiStatus.Error;
+                _instance.MidiError = midiError;
+                break;
+            case "noDevices":
+                _instance.MidiStatus = MidiStatus.NoDevices;
+                break;
+            case "connected":
+                _instance.MidiStatus = MidiStatus.Connected;
+                _instance.DeviceName = deviceName;
+                break;
+            default:
+                _instance.MidiStatus = MidiStatus.Error;
+                _instance.MidiError = "Unknown error.";
+                break;
+        }
+
+        var statusChanged = _instance.StatusChanged;
+        
+        if (statusChanged is not null)
+        {
+            statusChanged(_instance, EventArgs.Empty);
+        }
+    }
+
+    [JSInvokable]
+    public static void NotePlayed(int note)
+    {
+        if (_instance is null)
+        {
+            return;
+        }
+
+        var noteDown = _instance.NoteDown;
+
+        if (noteDown is not null)
+        {
+            noteDown(_instance, note);
+        }
+    }
+}
+
+public enum MidiStatus
+{
+    Ready,
+    Error,
+    NotSupported,
+    NoDevices,
+    Connected
+}
---
+++ BlueNotation/wwwroot/Scripts/MidiScript.js
@@ -0,0 +1,64 @@
+﻿let midiStatus = 'ready';
+let midiName = '';
+let midiError = '';
+
+function midiStart() {
+    if (WebMidi.supported) {
+        WebMidi.addListener('connected', debounce(updateDevice, 1000));
+        WebMidi.addListener('disconnected', debounce(updateDevice, 1000));
+
+        WebMidi
+            .enable()
+            .then(updateDevice)
+            .catch(err => onMidiError(err));
+    }
+    else {
+        midiStatus = 'unsupported';
+        statusUpdate();
+    }
+}
+
+function onMidiError(err) {
+    midiError = err;
+    midiStatus = 'error';
+    statusUpdate();
+}
+
+function statusUpdate() {
+    DotNet.invokeMethodAsync('BlueNotation', 'MidiEvent', midiStatus, midiName, midiError);
+}
+
+function updateDevice() {
+
+    midiStatus = 'noDevices';
+
+    if (WebMidi.inputs.length < 1) {
+        statusUpdate();
+        return;
+    }
+
+    midiStatus = 'connected';
+
+    const device = WebMidi.inputs[0];
+    device.removeListener();
+
+    midiName = device.name;
+
+    device.addListener("noteon", e => {
+        notePressed(e.data[1]);
+    });
+
+    statusUpdate();
+}
+
+function notePressed(note) {
+    DotNet.invokeMethodAsync('BlueNotation', 'NotePlayed', note);
+}
+
+function debounce(func, timeout = 300) {
+    let timer;
+    return (...args) => {
+        clearTimeout(timer);
+        timer = setTimeout(() => { func.apply(this, args); }, timeout);
+    };
+}
