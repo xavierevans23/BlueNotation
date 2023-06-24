8c57d2dada3b37e94c7a079b2986f68cf04013cb
Xavier Evans <xavierevans341@gmail.com>
2023-06-24 09:55:09 +0100

    Added stave component.

---
+++ BlueNotation/Shared/Stave.razor.cs
@@ -0,0 +1,46 @@
+﻿using BlueNotation.Music;
+using Microsoft.JSInterop;
+
+namespace BlueNotation.Shared;
+
+public partial class Stave
+{
+    private readonly string _divId;
+
+    private static readonly Dictionary<Accidental, string> AccidentalNames = new()
+    {
+        { Accidental.Natural, "n"},
+        { Accidental.Sharp, "#"},
+        { Accidental.Flat, "b"}
+    };
+
+    public Stave()
+    {
+        _divId = Guid.NewGuid().ToString();
+    }
+
+    public async Task SetDisplay(IEnumerable<Note>? notes = null, bool useTrebleClef = true, Key? key = null)
+    {
+        key ??= new(Letter.C, Accidental.Natural);
+
+        if (notes is null || !notes.Any())
+        {
+            notes = new List<Note>();
+        }
+        var notesList = notes.ToList();        
+        
+        notesList.Sort((a, b) => NoteHelper.GetMidi(a) - NoteHelper.GetMidi(b));       
+
+        var noteNames = new List<string>();
+        foreach (var note in notesList)
+        {
+            noteNames.Add($"{note.Letter}/{note.Octave}");
+        }
+
+        var keyString = $"{key.Value.Letter}{(key.Value.Accidental == Accidental.Natural ? "" : AccidentalNames[key.Value.Accidental])}";
+
+        var beatCount = notes.Any() ? 1 : 0;
+
+        await JS.InvokeVoidAsync("draw", _divId, noteNames, "q", beatCount, 4, "4/4", keyString, useTrebleClef ? "treble" : "bass");
+    }
+}
---
+++ BlueNotation/wwwroot/Scripts/NotationScript.js
@@ -0,0 +1,48 @@
+﻿function draw(elementID, noteNames, noteDuration, timeSignatureTop, timeSignatureBottom, timeSignatureString, keySignatureString, clef) {
+
+    clearBox(elementID);
+    const {
+        Renderer,
+        Stave,
+        StaveNote,
+        Voice,
+        Formatter
+    } = Vex.Flow;
+
+    const div = document.getElementById(elementID);
+    const renderer = new Renderer(div, Renderer.Backends.SVG);
+
+    renderer.resize(500, 200);
+    const context = renderer.getContext();
+
+    const stave = new Stave(40, 40, 400);
+
+    stave.addClef(clef).addTimeSignature(timeSignatureString);
+
+    new Vex.KeySignature(keySignatureString).addToStave(stave);
+    
+    stave.setContext(context).draw();
+    document.getElementById(elementID).firstChild.style = "width: 100%; height: 100%"
+
+    const staveNote = new StaveNote({
+        keys: noteNames,
+        duration: noteDuration,
+        clef: clef
+    });
+
+    const notes = [staveNote];    
+
+    const voice = new Voice({
+        num_beats: timeSignatureTop,
+        beat_value: timeSignatureBottom
+    });
+    voice.addTickables(notes);
+    
+    new Formatter().joinVoices([voice]).format([voice], 350);
+    
+    voice.draw(context, stave);    
+}
+
+function clearBox(elementID) {
+    document.getElementById(elementID).innerHTML = "";
+}
